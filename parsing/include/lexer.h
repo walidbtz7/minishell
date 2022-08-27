bc9ba3e4aaa1475ead1f3d8a884c9e98e402b73d
Walid Boutzougarte <wboutzou@e1r10p8.1337.ma>
2022-08-27 02:11:43 +0100

    add token struct , create a function that initiation a token

---
+++ parsing/include/lexer.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wboutzou <wboutzou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 18:05:52 by wboutzou          #+#    #+#             */
+/*   Updated: 2022/08/27 01:58:12 by wboutzou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+#endif
