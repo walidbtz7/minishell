fcc4120487ab0e0a5cb34dcf35eded1553f45532
Walid Boutzougarte <wboutzou@e1r10p8.1337.ma>
2022-08-27 02:20:22 +0100

    add lexer struct , create a function that create a lexer

--- parsing/include/lexer.h
+++ parsing/include/lexer.h
@@ -6,7 +6,7 @@
 /*   By: wboutzou <wboutzou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 18:05:52 by wboutzou          #+#    #+#             */
-/*   Updated: 2022/08/27 01:58:12 by wboutzou         ###   ########.fr       */
+/*   Updated: 2022/08/27 02:15:45 by wboutzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,12 @@
 #include <unistd.h>
 #include <limits.h>
 
+typedef struct lexer
+{
+    char *src;
+    char c;
+    unsigned int i;
+} t_lexer;
+
+t_lexer *init_lexer(char *src);
 #endif
